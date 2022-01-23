c645bf53614c0ffbe7872cbebcce8c1e92112971
MartinDelille <MartinDelille@users.noreply.github.com>
2022-01-23 23:16:21 +0000

    deploy: fe3c7677c401ee3f988e6bf510455cf19332053f

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,4 +1,9 @@
 var store = [{
+"title": "Des Plumes et des Touches",
+"excerpt":"20h35 : La pointe du stylo s’aventure timidement sur ces pages. Cela fait un bon moment que je n’ai pas aligné autant de caractères manuscrits à la suite. À lus longtemps encore remonte la dernière fois où j’ai laissé ma plume donner libre cours à mes idées, sans tabou ni...","categories": [],
+"tags": ["dev","ecriture"],
+"url": "/2009/01/18/des-plumes-et-des-touches/"
+},{
 "title": "Open Source: Seven years after",
 "excerpt":"This morning F…book reminded me that seven years ago I pushed my first open source project on Github. It was not a big project and it isn’t even used anymore, nobody contributed to it except a colleague to advertise its deprecation so it won’t last in the history of software...","categories": [],
 "tags": ["opensource","github"],
@@ -8,4 +13,9 @@ var store = [{
 "excerpt":"Je commence cet article en pleine conférence sur Zoom, une plate-forme de visioconférence très en vogue en cette période de confinement. Je ne suis pas un grand fan de la start up nation de notre cher président et lorsqu’il s’agit d’utiliser un outil, je préfère toujours envisager les alternatives. Or...","categories": [],
 "tags": ["jitsi","teletravail"],
 "url": "/2020/05/02/visioconference-avec-jitsi/"
+},{
+"title": "RecordR",
+"excerpt":"Cette vidéo présentant l’un de mes dernières applications. ","categories": [],
+"tags": ["dubbing","dev"],
+"url": "/2021/03/31/recordr/"
 }]
