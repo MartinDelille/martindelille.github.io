a8660f46ca3c61f55aa984fed9490ba10d62a7cb
MartinDelille <MartinDelille@users.noreply.github.com>
2022-01-24 00:19:49 +0000

    deploy: 6e77ac2fef211c540811732f5e55e24e11657d92

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,12 +1,47 @@
 var store = [{
+"title": "Patamodelé",
+"excerpt":"    Ce petit court-métrage a été réalisé à l’occasion de la Semaine des Art de l’école CPE Lyon. Le matériel de base est la pâte à modelé mais d’autres fournitures ont été utilisées (papier, effet spéciaux, souvenir du mexique, …). ","categories": [],
+"tags": ["stop-motion","trucage","video"],
+"url": "/2002/06/01/patamodele/"
+},{
+"title": "La Croix du Cervin",
+"excerpt":"Ce court-métrage à été réalisé avec mon frère Thibaut Delille suite à la réalisation d’un premier court-métrage en pâte à modelé. Cette histoire tragique est tirée d’un recueil de nouvelles de haute montagne de Charles Gos. Le film est dédié à la mémoire de notre grand-père Pierre Delille alias “Grand...","categories": [],
+"tags": ["video","stop-motion"],
+"url": "/2006/01/01/la-croix-du-cervin/"
+},{
+"title": "Mémoires Vives",
+"excerpt":"    Ce projet est le fruit d’un atelier d’initiation au doublage organisé par Dominique Collignon-Maurin auquel j’ai participé en temps qu’intervenant technique. Le résultat a été projeté au festival de la Commune à Montreuil, musiciens et comédiens interprétant en direct grâce à mon système Joker. ","categories": [],
+"tags": ["dubbing","video","cappella"],
+"url": "/2008/09/01/memoires-vives/"
+},{
 "title": "Des Plumes et des Touches",
 "excerpt":"20h35 : La pointe du stylo s’aventure timidement sur ces pages. Cela fait un bon moment que je n’ai pas aligné autant de caractères manuscrits à la suite. À lus longtemps encore remonte la dernière fois où j’ai laissé ma plume donner libre cours à mes idées, sans tabou ni...","categories": [],
 "tags": ["dev","ecriture"],
 "url": "/2009/01/18/des-plumes-et-des-touches/"
 },{
+"title": "The Gopro Experiment",
+"excerpt":"    Lors de ce voyage au brésil, on a offert à mon ami Pierig le premier modèle de GoPro. On est revenu avec de quoi faire un film. ","categories": [],
+"tags": ["video"],
+"url": "/2011/10/01/the-gopro-experiment/"
+},{
+"title": "Le Ventilateur de la poussière",
+"excerpt":"    Film réalisé dans le cadre du festival 48 heures plus tard des Intergalactices à Lyon. ","categories": [],
+"tags": ["video"],
+"url": "/2018/04/16/le-ventilateur-de-la-poussiere/"
+},{
+"title": "Joker",
+"excerpt":"    Voici une vidéo générée avec le logiciel Gource présentant le développement de mon projet Joker. ","categories": [],
+"tags": ["dubbing","video","joker","opensource","github","dev"],
+"url": "/2018/07/18/joker-development/"
+},{
+"title": "DéTourNage",
+"excerpt":"    Ce projet est le fruit d’un atelier d’initiation au doublage organisé par Dominique Collignon-Maurin auquel j’ai participé en temps qu’intervenant technique et comédien. Le résultat a été projeté sur la place du marché d’Alligre dans le 12ème arrondissement, musiciens et comédiens interprétant en direct grâce au système Cappella. ","categories": [],
+"tags": ["dubbing","video","joker"],
+"url": "/2018/12/09/detournage/"
+},{
 "title": "Mon premier clip",
-"excerpt":"Voici un projet dont je suis pas peu fier: le clip de mon groupe de punk “Les Cocksuckers” dont j’ai fait la réalisation et le montage. ","categories": [],
-"tags": ["video","musique"],
+"excerpt":"Voici un projet dont je suis pas peu fier: le clip de mon groupe de punk “Les Cocksuckers” dont j’ai fait la réalisation et le montage. Ce projet fait usage de la technique du fond vert et du stop-motion. Production/direction artistique/story board : Les Cocksuckers Réalisation/montage : Martin Delille Assistante...","categories": [],
+"tags": ["video","musique","stop-motion","trucage"],
 "url": "/2019/12/19/mon-premier-clip/"
 },{
 "title": "Open Source: Seven years after",
@@ -14,10 +49,20 @@ var store = [{
 "tags": ["opensource","github"],
 "url": "/2020/04/12/open-source-seven-years-after/"
 },{
+"title": "Tuto pour partager son bureau avec un sac à patate",
+"excerpt":"Le confinement nous oblige à utiliser des outils 100% numérique pour travailler, et on regrette le temps de la réunion et du bon vieux papier crayon. Voici un moyen low tech pour pallier à ce problème. Vous aurez-besoin des éléments suivants: Bureau Système de visio conférence (type https://jitsi.org/) Téléphone avec...","categories": [],
+"tags": ["video","tuto","jitsi"],
+"url": "/2020/05/02/tuto-pour-partager-son-bureau-avec-un-sac-a-patate/"
+},{
 "title": "Visioconférence avec Jitsi: retour d'expérience",
 "excerpt":"Je commence cet article en pleine conférence sur Zoom, une plate-forme de visioconférence très en vogue en cette période de confinement. Je ne suis pas un grand fan de la start up nation de notre cher président et lorsqu’il s’agit d’utiliser un outil, je préfère toujours envisager les alternatives. Or...","categories": [],
 "tags": ["jitsi","teletravail"],
 "url": "/2020/05/02/visioconference-avec-jitsi/"
+},{
+"title": "Tuto pour trier ses playlist YouTube",
+"excerpt":"Tout est dans le titre. ","categories": [],
+"tags": ["video","tuto"],
+"url": "/2021/03/22/tuto-tri-playlist-youtube/"
 },{
 "title": "RecordR",
 "excerpt":"Cette vidéo présentant l’un de mes dernières applications. ","categories": [],
